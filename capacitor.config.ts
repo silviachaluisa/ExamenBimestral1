5d9b595be89dae540711bd448d5787c84e5b78ae
silviachaluisa <veronica-14-1998@hotmail.com>
2024-11-22 15:11:26 -0500

    Mi primer commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,27 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.examen.starter',
   appName: 'examen1',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+    
+      backgroundColor: 'linear-gradient(to right, #f05053, #e1eec3)', /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: "launch_screen",
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
